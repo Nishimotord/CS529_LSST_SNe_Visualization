0e2af3fc8d5273d0eaa692e27a293240bc988901
Ayadla <yadla1997@gmail.com>
2019-12-04 20:16:08 -0600

    camera reset and rings

--- src/js/scene.js
+++ src/js/scene.js
@@ -17,65 +17,103 @@ Built on Andrew Burk's Project 3 Base code
 var App = App || {};
 
 /* Create the scene class */
-const Scene = function(options) {
-  // setup the pointer to the scope 'this' variable
-  const self = this;
+const Scene = function (options) {
+    // setup the pointer to the scope 'this' variable
+    const self = this;
 
-  // scale the width and height to the screen size
-  const width = d3.select(".particleDiv").node().clientWidth;
-  const height = width / 2;
+    // scale the width and height to the screen size
+    const width = d3.select(".particleDiv").node().clientWidth;
+    const height = width / 2;
+    //var frustumSize = 600;
+    //var aspect = width / height;
+    self.scene = new THREE.Scene();
+  //  var cameraRig = new THREE.Group();
 
-  // create the scene
-  self.scene = new THREE.Scene();
+    //cameraRig.add(cameraOrtho);
+   
+    //self.scene.add(cameraRig);
+    // create the scene
 
-  // setup the camera
-  self.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, -1);
-  self.camera.position.set(20000, 20000, 20000);
-  self.camera.up = new THREE.Vector3(0, 1, 0);
-  self.camera.lookAt(0, 0, 0);
 
-  // Add a directional light to show off the objects
-  // Position the light out from the scene, pointing at the origin
+    //var cameraOrtho = new THREE.PerspectiveCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
+    //var cameraOrthoHelper = new THREE.CameraHelper(cameraOrtho);
 
-  // add the light to the camera and the camera to the scene
-  self.scene.add(self.camera);
+    //var rad = 1500;
+    //var geometry = new THREE.RingGeometry(1.9 * rad, 2.0 * rad, 64);
+    //var geometry1 = new THREE.RingGeometry(0.9 * rad, 1.0 * rad, 64);
+    ////, 5, 0, Math.PI * 2.
+    //var RingMaterial = new THREE.MeshBasicMaterial({
+    //    //map: ringTex,
+    //    side: THREE.DoubleSide,
+    //    transparent: true,
+    //    opacity: 0.5,
+    //});
+    //var ringMesh = new THREE.Mesh(geometry, RingMaterial);
+    //var ringMesh1 = new THREE.Mesh(geometry1, RingMaterial);
+    //cameraRig.add(ringMesh);
+    //cameraRig.add(ringMesh1);
+   // cameraRig.position.set(0, 0, 0);
+   // cameraOrthoHelper.controls.enableRotate=false;
+    //cameraRig.lookAt(0, 0, 0);
+    //self.controls.update();
+    //cameraRig.rotation=;
+    self.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, -1);
+    self.camera.position.set(20000, 20000, 20000);
+    self.camera.up = new THREE.Vector3(0, 1, 0);
+    self.camera.lookAt(0, 0, 0);
 
-  self.scene.background = new THREE.Color(0x000000);
-  // create the renderer
-  self.renderer = new THREE.WebGLRenderer();
+    // Add a directional light to show off the objects
+    // Position the light out from the scene, pointing at the origin
 
-  // set the size and append it to the document
-  self.renderer.setSize(width, height);
-  document
-    .getElementById(options.container)
-    .appendChild(self.renderer.domElement);
+    // add the light to the camera and the camera to the scene
+    self.scene.add(self.camera);
+  //  self.scene.add(cameraOrthoHelper);
 
-  // enable camera controls with OrbitControls library
-  self.controls = new THREE.OrbitControls(
-    self.camera,
-    self.renderer.domElement
-  );
-  // enables panning up/down instead of dolly_pan...
-  self.controls.screenSpacePanning = true;
-  //self.controls.update();
-  // expose the public functions
-  // Try on the console App.scene and you should see these
-  // three functions. Every other element acts as a private
-  // attribute or function. For more information, check
-  // javascript module patterns.
-  self.public = {
-    resize: function() {},
+    self.scene.background = new THREE.Color(0x000000);
+    // create the renderer
+    self.renderer = new THREE.WebGLRenderer();
 
-    addObject: function(obj) {
-      self.scene.add(obj);
-    },
+    // set the size and append it to the document
+    self.renderer.setSize(width, height);
+    document
+        .getElementById(options.container)
+        .appendChild(self.renderer.domElement);
 
-    render: function() {
-      requestAnimationFrame(self.public.render);
-      self.controls.update();
-      self.renderer.render(self.scene, self.camera);
+    // enable camera controls with OrbitControls library
+    self.controls = new THREE.OrbitControls(
+        self.camera,
+       // self.CameraHelper,
+        self.renderer.domElement
+
+    );
+    //var x = new THREE.OrbitControls(cameraOrtho)
+    //x.controls.enabled = false;
+    document.getElementById("reset").addEventListener("click", reset);
+    function reset() {
+        self.camera.position.set(20000, 20000, 20000);
+        self.camera.lookAt(0, 0, 0);
     }
-  };
+    // enables panning up/down instead of dolly_pan...
+    self.controls.screenSpacePanning = true;
+    //self.controls.update();
+    // expose the public functions
+    // Try on the console App.scene and you should see these
+    // three functions. Every other element acts as a private
+    // attribute or function. For more information, check
+    // javascript module patterns.
+    self.public = {
+        resize: function () { },
 
-  return self.public;
+        addObject: function (obj) {
+            self.scene.add(obj);
+        },
+
+        render: function () {
+            requestAnimationFrame(self.public.render);
+            self.controls.update();
+
+            self.renderer.render(self.scene, self.camera);
+        }
+    };
+    return self.public;
 };
